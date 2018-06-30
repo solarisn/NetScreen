4e68bd2e8fabcbeb7b050e8960bc94dac7f8f6c3
Emma <katarina.hawk@gmail.com>
2018-06-30 15:11:17 -0700

    updated index.js to use correct port and force forward to SSL version of site

--- aframenv/index.js
+++ aframenv/index.js
@@ -1,8 +1,46 @@
-const express = require('express')
-const app = express()
+// Imports
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+process.title = "some-bullshit"
+
+var app = express()
+app.use(serveStatic('.', {'index': ['index.html']}));
 
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
 
 app.listen(443, () => console.log('Server running on port 443'))
+
+
+let port;
+let webServer;
+if (process.env.SSL === "true") {
+  port = process.env.PORT || 443;
+  console.log("Starting server with SSL");
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/netscreen.3diq.io/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/netscreen.3diq.io/fullchain.pem")
+  };
+  webServer = https.createServer(options, app);
+} else {
+  port = process.env.PORT || 8080;
+  console.log("Starting server without SSL");
+  webServer = http.createServer(app);
+}
+
+
+//listen on port
+webServer.listen(port, function () {
+  console.log('listening on http://localhost:' + port);
+});
+
+if (process.env.SSL == 'true') {
+  http.createServer(function (req, res) {
+      res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+      res.end();
+  }).listen(80);
+}
