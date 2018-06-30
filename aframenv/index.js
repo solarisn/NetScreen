611cf437ac88a8e7b32e932669ab7920315b140b
Emma <katarina.hawk@gmail.com>
2018-06-30 14:49:08 -0700

    added index.js for handling requests

---
+++ aframenv/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+
+app.listen(3000, () => console.log('Server running on port 8000'))
